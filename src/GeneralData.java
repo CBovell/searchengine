d52fd900dfcbcef3cc4232384ac609dc487d1939
CBovell <venujaperera12@gmail.com>
2022-12-03 16:37:48 -0500

    db models

---
+++ src/GeneralData.java
@@ -0,0 +1,16 @@
+public abstract class GeneralData {
+
+    String title;
+    String url;
+
+    public GeneralData(){
+        this.title="";
+        this.url = "";
+    }
+
+    public GeneralData(String title, String url){
+        this.title=title;
+        this.url=url;
+
+    }
+}
---
+++ src/PageFiles.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class PageFiles extends GeneralData {
+
+    public HashMap<String, Integer> words;
+    public int totalWords;
+    public ArrayList<String> outgoingLinks;
+
+
+    public PageFiles(){
+        super();
+        this.words = new HashMap<>();
+        this.totalWords=0;
+        this.outgoingLinks = new ArrayList<String>();
+
+    }
+
+    public PageFiles(String title, String url, HashMap<String,Integer> words, int totalWords, ArrayList<String> outgoingLinks){
+        super(title, url);
+        this.words = words;
+        this.totalWords=totalWords;
+        this.outgoingLinks=outgoingLinks;
+
+    }
+
+}
