cd177794262fb4166c9b55ba73d65ea280c0ee48
CBovell <venujaperera12@gmail.com>
2022-12-05 20:55:15 -0500

    crawler progress

--- src/Crawler.java
+++ src/Crawler.java
@@ -2,10 +2,22 @@ import javax.xml.crypto.Data;
 import java.io.*;
 import java.lang.reflect.Array;
 import java.net.MalformedURLException;
-import java.util.ArrayList;
+import java.util.*;
 
 
 public class Crawler {
+
+    public HashMap<String, Integer> wordDoc;
+    public int numDocs;
+    public String seed;
+
+
+    public Crawler(String seed){
+        this.wordDoc = new HashMap<String, Integer>();
+        this.numDocs = 0;
+        this.seed = seed;
+
+    }
 
     public static void testOut(){
         try{
@@ -31,16 +43,38 @@ public class Crawler {
 
     }
 
-    public static String[] getWords(String content){
+    public HashMap<String, Integer> getWords(String content){
+        HashMap<String, Integer> toReturn = new HashMap<>();
         String words = (content.substring(content.indexOf("<p>")+4,content.indexOf("</p>")-1));
-        return words.split("\n");
+        String[] wordsList = words.split("\n");
+        ArrayList<String> wordsArray = new ArrayList<String>();
+
+        for(int i=0; i < wordsList.length;i++){
+            wordsArray.add(wordsList[i]);
+            if(toReturn.containsKey(wordsList[i]) == false){
+                if(this.wordDoc.containsKey(wordsList[i])){
+                    toReturn.put(wordsList[i], 1);
+                    this.wordDoc.put(wordsList[i], this.wordDoc.get(wordsList[i])+1);
+                }
+                else{
+                    toReturn.put(wordsList[i], 1);
+                    this.wordDoc.put(wordsList[i], 1);
+                }
+            }
+            else{
+                toReturn.put(wordsList[i], toReturn.get(wordsList[i])+1);
+            }
+        }
+        this.numDocs+=1;
+
+        return toReturn;
     }
 
-    public static String getTitle(String content){
+    public String getTitle(String content){
         return (content.substring(content.indexOf("<title>")+7,content.indexOf("</title>")));
     }
 
-    public static ArrayList<String> getOutgoingLinks(String content, String URL){
+    public ArrayList<String> getOutgoingLinks(String content, String URL){
         String prefix = URL.substring(0, URL.lastIndexOf("/")+1);
         String words = (content.substring(content.indexOf("</p>")+5,content.indexOf("</body>")-1));
         ArrayList<String> toReturn = new ArrayList<String>();
@@ -61,29 +95,36 @@ public class Crawler {
         return toReturn;
     }
 
-    //public static String[] getOutgoingLinks(String content){
-        //String words = (content.substring(content.indexOf("<p>")+4,content.indexOf("</p>")-1));
-        //return new String["joe"];
-    //}
+
+    public void proccessData(String URL){
+
+    }
+
+    public void crawl(){
+        Queue<String> q = new LinkedList<>();
+        q.add(this.seed);
+
+        while(q.isEmpty() == false){
+            try {
+                String content = WebRequester.readURL(q.peek());
+                ArrayList<String> links = (this.getOutgoingLinks(content, q.peek()));
+                for(int i=0;i< links.size();i++){
+                    q.add(links.get(i));
+                }
 
 
-
-    public static void main(String[] args){
-        try {
-            String content = WebRequester.readURL("http://people.scs.carleton.ca/~davidmckenney/tinyfruits/N-0.html");
-
-            System.out.println(content);
-            ArrayList<String> links = (Crawler.getOutgoingLinks(content, "http://people.scs.carleton.ca/~davidmckenney/tinyfruits/N-0.html"));
-            for(int i=0;i< links.size();i++){
-                System.out.println(links.get(i));
+            }catch(MalformedURLException e){
+                e.printStackTrace();
+                String toReAdd = q.remove();
+                q.add(toReAdd);
+            }catch(IOException e){
+                e.printStackTrace();
+                String toReAdd = q.remove();
+                q.add(toReAdd);
             }
 
-        }catch(MalformedURLException e){
-            e.printStackTrace();
-        }catch(IOException e){
-            e.printStackTrace();
-        }
 
+        }
 
 
     }
